37f28cf619cb2d7d2e67575601d67268e1bb7a4e
Emmanuel Desmornes <desmornes.ejr@gmail>
2026-03-15 20:31:43 -0400

    Pages Admin et Catalogue Fini

--- Page_Catalogue/Page-Catalogue/src/main.tsx
+++ Page_Catalogue/Page-Catalogue/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createRoot(document.getElementById('root')!).render(
+import CatalogueProduitPage from "./CatalogueProduitPage";
+import PageAdmin from "./PageAdmin";
+
+const router = createBrowserRouter([
+  { path: "/produits", element: <CatalogueProduitPage></CatalogueProduitPage> },
+  { path: "/PageAdmin", element: <PageAdmin></PageAdmin> },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
