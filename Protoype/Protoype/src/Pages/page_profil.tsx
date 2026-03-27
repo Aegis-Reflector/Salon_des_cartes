f4f78878af2d773b72b5cd855d2240bfc371174a
Jordan Ly <jordanly0109@gmail.com>
2026-03-27 10:48:55 -0400

    Ajout des liens

--- Protoype/Protoype/src/Pages/page_profil.tsx
+++ Protoype/Protoype/src/Pages/page_profil.tsx
@@ -5,13 +5,41 @@ export default function PageProfil() {
         <div className="row min-vh-100 g-0 mx-0">
           <div className="col-2 col-sm-3 col-xl-2 bg-dark">
             <div className="container">
-              <nav className="navbar navbar-dark bg-dark">
+              <nav className="navbar navbar-dark bg-dark border-bottom border-white">
                 <div className="container-fluid">
                   <a className="navbar-brand" href="#">
                     Navbar
                   </a>
                 </div>
               </nav>
+
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Active
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Link
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
 
@@ -22,13 +50,20 @@ export default function PageProfil() {
                   <ul className="navbar-nav ms-auto">
                     <li className="nav-item">
                       <a className="nav-link" href="#">
-                        {" "}
                         <i className="bi bi-arrow-bar-right me-2" />
+                        Logout
                       </a>
                     </li>
                   </ul>
                 </div>
               </nav>
+
+              <div className="px-3">
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
+              </div>
             </div>
           </div>
         </div>
